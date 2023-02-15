c7882cf01fac01b0c9dcff55070a8135b2c7a442
ilyasaftr <ilyasarecord@gmail.com>
2023-02-15 13:49:48 +0700

    remove @tensorflow/tfjs-node-gpu

--- models/v3_NSFWModel.js
+++ models/v3_NSFWModel.js
@@ -10,10 +10,10 @@ if (isLinux) {
     haveAVX = cpuInfo.includes("avx");
 }
 
-// if (!haveAVX) {
-//     //console.error(cpuInfo);
-//     console.error("WARNING!!!!!!!! AVX instruction set not detected");
-// }
+if (!haveAVX) {
+    //console.error(cpuInfo);
+    //console.error("WARNING!!!!!!!! AVX instruction set not detected");
+}
 
 const axios = require("axios")
 const Path = require("path");
@@ -42,12 +42,13 @@ if (!!process.env.CACHE_IMAGE_HASH_FILE) {
 
 let tf, nsfw = {};
 //check if we have GPU
-if (process.env.GPU) {
-    tf = require('@tensorflow/tfjs-node-gpu');
-    console.log("Using GPU");
-} else {
-    tf = require("@tensorflow/tfjs-node");
-}
+// if (process.env.GPU) {
+//     tf = require('@tensorflow/tfjs-node-gpu');
+//     console.log("Using GPU");
+// } else {
+//     tf = require("@tensorflow/tfjs-node");
+// }
+tf = require("@tensorflow/tfjs-node");
 nsfw = require("nsfwjs");
 tf.enableProdMode(); // enable on production
 
